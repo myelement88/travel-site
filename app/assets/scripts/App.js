1e4d0c66c20e9ab56b848375c7a88cf76ed0b6c9
Georg <myelement88@gmail.com>
2016-12-08 22:14:58 +0100

    Abschnitt 14, Lektion 40

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,12 @@
+var $ = require('jquery');
 var Person = require('./modules/Person');
+
+alert("Testing 12321 ABC");
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
 var jane = new Person("Jane Smith", "green");
 jane.greet();
+
+$("h1").remove();
