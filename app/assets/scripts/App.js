9108402b5345c3276433eeb8eae7c02a38305a5f
Georg <myelement88@gmail.com>
2016-12-08 22:35:48 +0100

    Abschnitt 14, Lektion 40 finished!

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,17 @@
 var $ = require('jquery');
-var Person = require('./modules/Person');
+import Person from './modules/Person';
 
-alert("Testing 12321 ABC");
+class Adult extends Person {
+    payTaxes() {
+        console.log(this.name + " now owes $0 in taxes.");
+    }
+}
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
-var jane = new Person("Jane Smith", "green");
+var jane = new Adult("Jane Smith", "pink");
 jane.greet();
+jane.payTaxes();
 
 $("h1").remove();
