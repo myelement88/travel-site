817fd86d073ed2d485ddfeb40857b4811c864018
Georg <myelement88@gmail.com>
2016-12-10 12:45:27 +0100

    Abschnitt 16, Lektion 45 finished!

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -2,8 +2,9 @@ import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class RevealOnScroll {
-    constructor() {
-        this.itemsToReveal = $(".feature-item");
+    constructor(els, offset) {
+        this.itemsToReveal = els;
+        this.offsetPercentage = offset;
         this.hideInitially();
         this.createWaypoints();
     }
@@ -13,14 +14,15 @@ class RevealOnScroll {
     }
 
     createWaypoints() {
-        this.itemsToReveal.each(function() {
+        var that = this;
+        this.itemsToReveal.each(function () {
             var currentItem = this;
             new Waypoint({
                 element: currentItem,
-                handler: function() {
+                handler: function () {
                     $(currentItem).addClass("reveal-item--is-visible");
                 },
-                offset: "85%"
+                offset: that.offsetPercentage
             });
         });
     }
